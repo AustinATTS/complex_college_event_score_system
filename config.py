9c2e4f36d6ef991621d9089a0c817dc39316a1a8
austinatts <austinatts@engineer.com>
2024-06-27 09:52:48 +0100

    AAdded the configuration settings for the application.

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATABASE_PATH = os.path.join(BASE_DIR, 'db', 'database.db')
+LOG_FILE_PATH = os.path.join(BASE_DIR, 'logging', 'app.log')
