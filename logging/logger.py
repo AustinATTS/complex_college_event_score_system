cc2d3b0581c353e9114b435e01125798a4913b7c
austinatts <austinatts@engineer.com>
2024-06-27 10:03:33 +0100

    Updated to integrate with logger template page

--- logging/logger.py
+++ logging/logger.py
@@ -1,8 +1,11 @@
 import logging
-from config import LOG_FILE_PATH
-
-logging.basicConfig(filename=LOG_FILE_PATH, level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 def get_logger(name):
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+    if not logger.hasHandlers():
+        handler = logging.FileHandler('app.log')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+    return logger
