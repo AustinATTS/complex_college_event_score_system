5214719d2b1106c5b48d9b62d3f0d9bbef5be716
austinatts <austinatts@engineer.com>
2024-06-27 12:03:09 +0100

    Added functionality for using CTkMessagebox windows thanks to Akascape

---
+++ utils/ctk_custom.py
@@ -0,0 +1,7 @@
+from CTkMessagebox import CTkMessagebox
+
+def show_error(title, message):
+    CTkMessagebox(title=title, message=message)
+
+def show_info(title, message):
+    CTkMessagebox(title=title, message=message)
