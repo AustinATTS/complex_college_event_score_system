83803c29e5c0054ba2cd012d1da5bf1b100323e2
austinatts <austinatts@engineer.com>
2024-06-30 13:35:58 +0100

    Some Moede Updates

--- models/participant.py
+++ models/participant.py
@@ -1,10 +1,23 @@
-class Participant:
-    def __init__(self, id, name, email, phone, team_id, role, password_hash):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.team_id = team_id
-        self.role = role
-        self.password_hash = password_hash
+import sqlite3
+from db.database import create_connection
 
+def add_participant():
+    pass
+
+def save_participant():
+    pass
+
+def delete_participant(participant_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM participants WHERE ID = ?", (participant_id))
+    cursor.commit()
+    conn.close()()
+
+def get_participant():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM participants")
+    participants = cursor.fetchall()
+    conn.close()
+    return participants
